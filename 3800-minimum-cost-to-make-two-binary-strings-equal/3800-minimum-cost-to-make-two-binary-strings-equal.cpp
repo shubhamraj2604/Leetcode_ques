1a15189a16886f6664e75f888aeb72355508d50f
Shubham Raj <shubhamraj2613@gmail.com>
2026-07-03 08:41:30 +0530

    Time: 8 ms (44.12%), Space: 21.3 MB (76.47%) - LeetHub

--- 3800-minimum-cost-to-make-two-binary-strings-equal/3800-minimum-cost-to-make-two-binary-strings-equal.cpp
+++ 3800-minimum-cost-to-make-two-binary-strings-equal/3800-minimum-cost-to-make-two-binary-strings-equal.cpp
@@ -12,21 +12,52 @@ public:
          }
          
          long long ans = 0;
-         int maxi = max(c01 , c10);
+    //      int maxi = max(c01 , c10);
          
-         int mini = min(c01 , c10);
+    //      int mini = min(c01 , c10);
 
-         ans+=min({2LL * mini * flipCost , 1LL * mini * swapCost});
+    //      ans+=min({2LL * mini * flipCost , 1LL * mini * swapCost});
 
-         maxi-=mini;
+    //      maxi-=mini;
 
-         ans+= min(1LL * (crossCost + swapCost) * (maxi/2) , 2LL * flipCost * (maxi/2));
+    //      ans+= min(1LL * (crossCost + swapCost) * (maxi/2) , 2LL * flipCost * (maxi/2));
          
 
-         if(maxi % 2 == 1){
-            ans+=1LL * flipCost;
-         }
+    //      if(maxi % 2 == 1){
+    //         ans+=1LL * flipCost;
+    //      }
+
+
+            // i have two choices
+            //flip or swap
+            // flip means i will have all of them 10 
+            //swap
+            if(2LL * flipCost > 1LL * swapCost){
+                //swap
+                int mini = min(c01 , c10);
+                int maxi = max(c01 , c10);
+                ans+=1LL * mini * swapCost;
+                maxi-=mini;
+
+                if((swapCost + crossCost) < 2LL * flipCost){
+                    //cross
+                     ans+=(swapCost + crossCost) * 1LL * (maxi/2);
+                    // if any left after cross then flip
+                     if(maxi % 2 == 1){
+                        //flip
+                        ans+=flipCost;
+                     }
+                }else{
+                    //if flip is better than cross then take this
+                    ans+=1LL*maxi*flipCost;
+                }
+                
+            }else{
+                //flip is better than swap + remaining operation then this
+                ans+=1LL * (c01 + c10) * flipCost;
+            }
 
          return ans;
+    
     }
 };
