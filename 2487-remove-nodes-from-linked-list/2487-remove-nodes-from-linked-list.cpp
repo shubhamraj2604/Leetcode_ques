4332fee56b91761b22cb423986aaeca4e6034da7
Shubham Raj <shubhamraj2613@gmail.com>
2026-09-19 20:31:53 +0530

    Time: 15 ms (42.38%), Space: 173.6 MB (29.34%) - LeetHub

--- 2487-remove-nodes-from-linked-list/2487-remove-nodes-from-linked-list.cpp
+++ 2487-remove-nodes-from-linked-list/2487-remove-nodes-from-linked-list.cpp
@@ -10,27 +10,57 @@
  */
 class Solution {
 public:
-    ListNode* solve(ListNode* head , stack<int>&st){
-        if(head->next == NULL){
-           st.push(head->val);
-           return head;
-       }
+    // ListNode* solve(ListNode* head , stack<int>&st){
+    //     if(head->next == NULL){
+    //        st.push(head->val);
+    //        return head;
+    //    }
 
-       ListNode* nxtnode = solve(head->next , st);
-       if(!st.empty() && st.top() <= head->val){
-          st.pop();
-          st.push(head->val);
-          head->next = nxtnode;
-          return head;
-       }
+    //    ListNode* nxtnode = solve(head->next , st);
+    //    if(!st.empty() && st.top() <= head->val){
+    //       st.pop();
+    //       st.push(head->val);
+    //       head->next = nxtnode;
+    //       return head;
+    //    }
 
-       return nxtnode;
-    }
+    //    return nxtnode;
+    // }
     ListNode* removeNodes(ListNode* head) {
-       if(head == nullptr){
-         return nullptr;
-       }
-        stack<int>st;
-       return solve(head , st);
+    //    if(head == nullptr){
+    //      return nullptr;
+    //    }
+    //     stack<int>st;
+    //    return solve(head , st);
+    stack<ListNode*>st;
+    while(head){
+        while(!st.empty() && st.top()->val < head->val){
+                st.pop();
+            }
+        st.push(head);
+        head = head->next;
+    }
+
+      ListNode* dummy = new ListNode(0);
+      ListNode* curr = dummy;
+
+      while(!st.empty()){
+          ListNode* top = st.top();
+          st.pop();
+          curr->next = top;
+          curr = curr->next;
+      }
+      curr->next = nullptr;
+      // reverse the list;
+
+      ListNode* prev = nullptr;
+      curr = dummy->next;
+      while(curr){
+         ListNode* nxtnode = curr->next;
+         curr->next = prev;
+         prev = curr;
+         curr = nxtnode;
+      }
+      return prev;
     }
 };
